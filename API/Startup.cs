ec920b7290a064157bacff408be447bd98cf8a95
John Sourvinos <johnsourvinos@hotmail.com>
2025-09-06 08:41:02 +0300

    Removes security headers

--- API/Startup.cs
+++ API/Startup.cs
@@ -130,6 +130,7 @@ namespace API {
 
         public virtual void Configure(IApplicationBuilder app) {
             app.UseMiddleware<ResponseMiddleware>();
+            app.UseMiddleware<SecurityHeadersMiddleware>();
             app.UseDefaultFiles();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
